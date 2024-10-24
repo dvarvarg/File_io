ed1a99d029cda4bb044e24afaea46b7c020cb5e4
dvarvarg <dvarvarg@gmail.com>
2024-10-25 01:14:54 +0300

    Создали функцию save_history для сохранения истории в файл формата json

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 from tkinter import *
 import requests
 from tkinter import filedialog as fd
-from tkinter import ttk
+#from tkinter import ttk
 from tkinter import messagebox as mb
+import pyperclip
+import os
+import json
 
-from PyInstaller.lib.modulegraph.modulegraph import entry
+
+file_save='history_link.json'
+
+
+def save_history(file_save, link,file): # куда и что сохраняем
+    history=[] #создали пустой список
+    if os.path.exists(file_save):  # если файл существует
+        with open(file_save,'r') as f: # и дополняем его новым
+            history=json.load(f) # то мы загружаем в него все данные из этого файла
+    history.append({'name_file' : os.path.basename(file), # имя файла
+                    'save_file_history':os.path.basename(file_save), # куда мы сохраняем
+                    'link': link}) # сгенерированная ссылка
+    with open(file_save, 'w') as f:
+        json.dump(history, f, indent=4) # загрузить данные (какие, куда, с каким отступом)
 
 
 def get_response():
-    file=fd.askopenfilename()
-    if file:
-        f={'file':open(file,'rb')} #rb-чтение файла побитно
-        answer_json=requests.post('https://file.io',files=f)
-        print(answer_json.json())
-        if answer_json.status_code==404:
-            mb.showerror('Ошибка','Ошибка 404. Неверно указанный путь к сайту')
-        if answer_json.status_code == 200:
-            link=answer_json.json()['link']
-            e.insert(0,link)
-
-
+    try:
+        file=fd.askopenfilename()
+        if file:
+            with open (file,'rb') as fi:  # rb-чтение файла побитно
+                f={'file':fi}
+                answer_json=requests.post('https://file.io',files=f)
+                print(answer_json.json())
+                if answer_json.status_code == 200:
+                    link=answer_json.json()['link']
+                    e.delete(0,END)
+                    e.insert(0,link)
+                    pyperclip.copy(link) #сохранение в буфер обмена, как только получили ссылку, то она у нас уже скопирована и ее можно уже вставлять
+                    save_history(file_save, link,file)
+                else:
+                     mb.showerror('Ошибка', 'Неверно указанный путь к сайту')
+    except Exception as e:
+        mb.showerror('Ошибка',f'Произошла ошибка: {e}')
 
 
 window=Tk()
